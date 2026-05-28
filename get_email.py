b0cdaa36e95141c7376477ada0fb1e8f80724702
Stephanie <110360031+Stephaniew1@users.noreply.github.com>
2026-05-29 06:21:00 +1000

    Update get_email.py

--- get_email.py
+++ get_email.py
@@ -3,25 +3,19 @@ import google_auth_oauthlib.flow
 import google.auth.transport.requests
 from google.oauth2.credentials import Credentials
 
-# Scope: "gmail.send" means we only need to send
-# .
 SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
 
 def main():
     creds = None
-
-    # If we already have token.json, try to load it
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
-    # If no valid creds or token is invalid/expired:
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            # Refresh
+          
             creds.refresh(google.auth.transport.requests.Request())
         else:
-            # Do an interactive OAuth flow in your browser
             flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
             )
